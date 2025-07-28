af43617e5888ab6ed112cf389c032bbb118ac3eb
Pierre Raybaut <pierre.raybaut@gmail.com>
2025-07-28 21:23:16 +0200

    Replace CDL by Sigima in minor parts of the project

--- scripts/_tests_.bat
+++ scripts/_tests_.bat
@@ -3,7 +3,7 @@ REM ======================================================
 REM Generic Python Installer build script
 REM ------------------------------------------
 REM Licensed under the terms of the BSD 3-Clause
-REM (see cdl/LICENSE for details)
+REM (see sigima/LICENSE for details)
 REM ======================================================
 call %~dp0utils GetScriptPath SCRIPTPATH
 call %FUNC% GetLibName LIBNAME
@@ -11,11 +11,11 @@ call %FUNC% SetPythonPath
 call %FUNC% UsePython
 call %FUNC% GetVersion VERSION
 call %FUNC% GetModName MODNAME
-call %FUNC% GetCDLProjectPath CDL_MODULE_PATH
+call %FUNC% GetSigimaProjectPath SIGIMA_MODULE_PATH
 
 echo Modname=%MODNAME%
 echo LibName=%LIBNAME%
 echo LibVersion=%VERSION%
-echo CDLModulePath=%CDL_MODULE_PATH%
+echo SigimaModulePath=%SIGIMA_MODULE_PATH%
 
 call %FUNC% EndOfScript
